f572aba55f5bbe8910dcd1f16d792263d3e53504
Sujoy Paul <freelancersujoy@gmail.com>
2024-04-14 01:28:45 +0600

    Edit mode and view mode

--- profile-view/src/Pages/EditProfile.jsx
+++ profile-view/src/Pages/EditProfile.jsx
@@ -15,6 +15,7 @@ const EditProfile = () => {
     const [serverMsg, setServerMsg] = useState(null)
     const user = JSON.parse(localStorage.getItem('userinfo'))[0]
     const [imglink, setImgLink] = useState(null)
+    const [editMode, setEditMode] = useState(false)
 
     const [userProfile, setUserProfile] = useState([])
     
@@ -26,6 +27,22 @@ const EditProfile = () => {
             setViewImg(img)
         }
     }
+
+    // user informations
+    const {
+        bio, 
+        name, 
+        github_link,
+        twitter_link,
+        portfolio_link,
+        hackerRank_link,
+        profile_pic, 
+        drible_link,
+        linkedin_link,
+        facebook_link,
+        codeForce_link,
+        instagram_link,
+    } = userProfile
     
     const handleSaveProfile = (e) => {
         e.preventDefault()
@@ -46,16 +63,16 @@ const EditProfile = () => {
             username: user.username,
             name: name,
             bio: bio,
-            profile_pic: imglink,
-            github_link: github,
-            portfolio_link: portfolio,
-            hackerRank_link: hackerRank,
-            codeForce_link: codeForce,
-            drible_link: drible,
-            linkedin_link: linkedin,
-            facebook_link: facebook,
-            instagram_link: instagram,
-            twitter_link: twitter
+            profile_pic: imglink ? imglink : profile_pic,
+            github: github,
+            portfolio: portfolio,
+            hackerRank: hackerRank,
+            codeForce: codeForce,
+            drible: drible,
+            linkedin: linkedin,
+            facebook: facebook,
+            instagram: instagram,
+            twitter: twitter
         }
         // console.log(userdetails)
         
@@ -123,28 +140,17 @@ const EditProfile = () => {
     })
     }
 
-    // user informations
-    const {
-        bio, 
-        name, 
-        github,
-        twitter,
-        portfolio,
-        hackerRank,
-        profile_pic, 
-        driblehackerRank,
-        linkedinhackerRank,
-        facebookhackerRank,
-        codeForcehackerRank,
-        instagramhackerRank,
-    } = userProfile
+    
     
     const buttonBg = 'bg-zinc-900 border-2 border-slate-500 border-dashed '
     return (
         <div className='min-h-screen pb-20'>
-            <Container>
-                        <div className={`text-center sticky top-0 ${callServer ? 'bg-green-700': 'bg-red-700'} py-1 rounded-b-md  text-white`}>
-                            <p className='text-sm'>{callServer ? `${serverMsg}` : 'Edit mode enabled - Tap to view mode'}</p>
+            {
+                editMode ?
+                <Container>
+                        {/* <div className={`text-center sticky top-0 ${callServer ? 'bg-green-700': 'bg-red-700'} ${editMode ? 'bg-red-700' : ' bg-green-700'} py-1 rounded-b-md  text-white`}> */}
+                        <div className={`text-center sticky top-0 ${editMode ? 'bg-red-700' : ' bg-green-700'} py-1 rounded-b-md  text-white`}>
+                            <p className='text-sm'>{callServer ? `${serverMsg}` : `${editMode ? 'Edit mode enabled' : 'View mode enabled'}`}</p>
                         </div>
                 <div className='flex md:flex-row flex-col gap-5 mt-10 p-5 md:p-0'>
                     <div>
@@ -163,7 +169,11 @@ const EditProfile = () => {
                     <div className='w-full '>
                         <p className='py-2 ml-2'>Information section</p>
                         <form onSubmit={handleSaveProfile}>
-                        <div className='bg-zinc-900 border-2 border-slate-500 border-dashed rounded-md w-full p-8 '>
+                        <div className='bg-zinc-900 border-2 border-slate-500 border-dashed rounded-md w-full px-8 p-8'>
+                            <div className='flex justify-end block'>
+                                <input type="checkbox" onClick={() => setEditMode(!editMode)} className="toggle "  />
+                            </div>
+
                             <input name='name' placeholder='your name...' type="text" className='py-2 bg-transparent outline-none text-4xl uppercase' defaultValue={name && name} /> <br />
                             <input name='bio' placeholder='something about your self...' type="text" className='bg-transparent outline-none text-2xl w-full mb-3' defaultValue={'Education makes a man to live in live'} />
                             <hr />
@@ -171,47 +181,47 @@ const EditProfile = () => {
                                 <div className='mt-5 md:w-[100%]'>
                                     <div className='flex items-center border-black'>
                                         <label htmlFor="github" className='text-2xl py-2 text-white bg-black px-8'><FaGithub /></label>
-                                        <input className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your github id' id='github' name='github' type="url" />
+                                        <input defaultValue={github_link} className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your github id' id='github' name='github' type="url" />
                                     </div>
 
                                     <div className='flex items-center border-[#cbcbcb] mt-4'>
                                         <label htmlFor="portfolio" className='text-2xl py-2 text-black bg-[#cbcbcb] px-8'><CgWebsite /></label>
-                                        <input className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your portfolio' id='portfolio' name='portfolio' type="url" />
+                                        <input defaultValue={portfolio_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your portfolio' id='portfolio' name='portfolio' type="url" />
                                     </div>
 
                                     <div className='flex items-center border-[#000] mt-4'>
                                         <label htmlFor="hackerRank" className='text-2xl py-2 text-white bg-[#000] px-8'><FaHackerrank /></label>
-                                        <input className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your hackerRank id' id='hackerRank' name='hackerRank' type="url" />
+                                        <input defaultValue={hackerRank_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your hackerRank id' id='hackerRank' name='hackerRank' type="url" />
                                     </div>
 
                                     <div className='flex items-center border-[#cbcbcb] mt-4'>
                                         <label htmlFor="codeForce" className='text-2xl py-2 text-black bg-[#cbcbcb] px-8'><SiCodeforces /></label>
-                                        <input className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your codeForce id' id='codeForce' name='codeForce' type="url" />
+                                        <input defaultValue={codeForce_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your codeForce id' id='codeForce' name='codeForce' type="url" />
                                     </div>
 
                                     <div className='flex items-center border-[#000] mt-4'>
                                         <label htmlFor="drible" className='text-2xl py-2 text-white bg-[#000] px-8'><FaDribbble /></label>
-                                        <input className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your drible id' id='drible' name='drible' type="url" />
+                                        <input defaultValue={drible_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your drible id' id='drible' name='drible' type="url" />
                                     </div>
 
                                     <div className='flex items-center border-[#cbcbcb] mt-4'>
                                         <label htmlFor="linkedin" className='text-2xl py-2 text-black bg-[#cbcbcb] px-8'><FaLinkedinIn /></label>
-                                        <input className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your linkedin id' id='linkedin' name='linkedin' type="url" />
+                                        <input defaultValue={linkedin_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your linkedin id' id='linkedin' name='linkedin' type="url" />
                                     </div>
 
                                     <div className='flex items-center border-[#000] mt-4'>
                                         <label htmlFor="facebook" className='text-2xl py-2 text-white bg-[#000] px-8'><FaFacebook /></label>
-                                        <input className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your facebook id' id='facebook' name='facebook' type="url" />
+                                        <input defaultValue={facebook_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your facebook id' id='facebook' name='facebook' type="url" />
                                     </div>
 
                                     <div className='flex items-center border-[#cbcbcb] mt-4'>
                                         <label htmlFor="instagram" className='text-2xl py-2 text-[#000] bg-[#cbcbcb] px-8'><FaInstagram /></label>
-                                        <input className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your instagram id' id='instagram' name='instagram' type="url" />
+                                        <input defaultValue={instagram_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your instagram id' id='instagram' name='instagram' type="url" />
                                     </div>
 
                                     <div className='flex items-center border-[#000] mt-4'>
                                         <label htmlFor="twitter" className='text-2xl py-2 text-white bg-[#000] px-8'><FaTwitter /></label>
-                                        <input className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your twitter id' id='twitter' name='twitter' type="url" />
+                                        <input defaultValue={twitter_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your twitter id' id='twitter' name='twitter' type="url" />
                                     </div>
 
                                     <button className='bg-black hover:bg-gray-950 duration-200 py-2 mt-5 w-full' type="submit ">Save</button>
@@ -221,6 +231,94 @@ const EditProfile = () => {
                     </div>
                 </div>
             </Container>
+
+            :
+
+            <Container>
+                        {/* <div className={`text-center sticky top-0 ${callServer ? 'bg-green-700': 'bg-red-700'} ${editMode ? 'bg-red-700' : ' bg-green-700'} py-1 rounded-b-md  text-white`}> */}
+                        <div className={`text-center sticky top-0 ${editMode ? 'bg-red-700' : ' bg-green-700'} py-1 rounded-b-md  text-white`}>
+                            <p className='text-sm'>{callServer ? `${serverMsg}` : `${editMode ? 'Edit mode enabled' : 'View mode enabled'}`}</p>
+                        </div>
+                <div className='flex md:flex-row flex-col gap-5 mt-10 p-5 md:p-0'>
+                    <div>
+                        <p className='py-2 ml-2'>Image section</p>
+                        <label htmlFor="profile_pic" >
+                            <div className={`w-[300px] h-[300px] overflow-hidden ${buttonBg} rounded-lg`}>
+                                <img src={viewImg ? viewImg : profile_pic} alt="" />
+                            </div>
+                            {/* <button className={`uppercase w-full py-2 btn-bg ${buttonBg} mt-1 rounded-b-lg`}> add picture</button> */}
+                        </label>
+                        <input disabled onChange={handleGetPhoto} id='profile_pic' className='hidden' type="file" />
+                        {/* <button onClick={handleImageChange}>Upload</button> */}
+                    </div>
+
+
+                    <div className='w-full '>
+                        <p className='py-2 ml-2'>Information section</p>
+                        <form onSubmit={handleSaveProfile}>
+                        <div className='bg-zinc-900 border-2 border-slate-500 border-dashed rounded-md w-full px-8 p-8'>
+                            <div className='flex justify-end block'>
+                                <input type="checkbox" onClick={() => setEditMode(!editMode)} className="toggle "  />
+                            </div>
+
+                            <input disabled name='name' placeholder='your name...' type="text" className='py-2 bg-transparent outline-none text-4xl uppercase' defaultValue={name && name} /> <br />
+                            <input disabled name='bio' placeholder='something about your self...' type="text" className='bg-transparent outline-none text-2xl w-full mb-3' defaultValue={'Education makes a man to live in live'} />
+                            <hr />
+
+                                <div className='mt-5 md:w-[100%]'>
+                                    <div className='flex items-center border-black'>
+                                        <label htmlFor="github" className='text-2xl py-2 text-white bg-black px-8'><FaGithub /></label>
+                                        <input disabled defaultValue={github_link} className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your github id' id='github' name='github' type="url" />
+                                    </div>
+
+                                    <div className='flex items-center border-[#cbcbcb] mt-4'>
+                                        <label htmlFor="portfolio" className='text-2xl py-2 text-black bg-[#cbcbcb] px-8'><CgWebsite /></label>
+                                        <input disabled defaultValue={portfolio_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your portfolio' id='portfolio' name='portfolio' type="url" />
+                                    </div>
+
+                                    <div className='flex items-center border-[#000] mt-4'>
+                                        <label htmlFor="hackerRank" className='text-2xl py-2 text-white bg-[#000] px-8'><FaHackerrank /></label>
+                                        <input disabled defaultValue={hackerRank_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your hackerRank id' id='hackerRank' name='hackerRank' type="url" />
+                                    </div>
+
+                                    <div className='flex items-center border-[#cbcbcb] mt-4'>
+                                        <label htmlFor="codeForce" className='text-2xl py-2 text-black bg-[#cbcbcb] px-8'><SiCodeforces /></label>
+                                        <input disabled defaultValue={codeForce_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your codeForce id' id='codeForce' name='codeForce' type="url" />
+                                    </div>
+
+                                    <div className='flex items-center border-[#000] mt-4'>
+                                        <label htmlFor="drible" className='text-2xl py-2 text-white bg-[#000] px-8'><FaDribbble /></label>
+                                        <input disabled defaultValue={drible_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your drible id' id='drible' name='drible' type="url" />
+                                    </div>
+
+                                    <div className='flex items-center border-[#cbcbcb] mt-4'>
+                                        <label htmlFor="linkedin" className='text-2xl py-2 text-black bg-[#cbcbcb] px-8'><FaLinkedinIn /></label>
+                                        <input disabled defaultValue={linkedin_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your linkedin id' id='linkedin' name='linkedin' type="url" />
+                                    </div>
+
+                                    <div className='flex items-center border-[#000] mt-4'>
+                                        <label htmlFor="facebook" className='text-2xl py-2 text-white bg-[#000] px-8'><FaFacebook /></label>
+                                        <input disabled defaultValue={facebook_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your facebook id' id='facebook' name='facebook' type="url" />
+                                    </div>
+
+                                    <div className='flex items-center border-[#cbcbcb] mt-4'>
+                                        <label htmlFor="instagram" className='text-2xl py-2 text-[#000] bg-[#cbcbcb] px-8'><FaInstagram /></label>
+                                        <input disabled defaultValue={instagram_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your instagram id' id='instagram' name='instagram' type="url" />
+                                    </div>
+
+                                    <div className='flex items-center border-[#000] mt-4'>
+                                        <label htmlFor="twitter" className='text-2xl py-2 text-white bg-[#000] px-8'><FaTwitter /></label>
+                                        <input disabled defaultValue={twitter_link}  className='py-2 text-md bg-[#212121] outline-none pl-4 w-full' placeholder='your twitter id' id='twitter' name='twitter' type="url" />
+                                    </div>
+
+                                    <button className='bg-black hover:bg-gray-950 duration-200 py-2 mt-5 w-full' type="submit ">Save</button>
+                                </div>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </Container>
+            }
         </div>
     );
 };
