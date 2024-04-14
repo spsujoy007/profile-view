8cc24f570dba8faa3ea21ec30896deb037ffed78
Sujoy Paul <freelancersujoy@gmail.com>
2024-04-14 22:47:27 +0600

    userAccountinfo api

--- profile-view backend/index.js
+++ profile-view backend/index.js
@@ -38,56 +38,68 @@ async function run(){
           return res.send([result, userinfo])
         }
       })
-
+      
+      
       app.post('/saveprofile', async(req, res) => {
-          const userdata = req.body
-          const existUserInUsers = await usersCollection.findOne({username: userdata.username})
-          if(existUserInUsers){
-            const existUserInProfile = await userProfileCollection.findOne({username: userdata.username})
-            if(existUserInProfile){
-              const filter = {username: userdata.username}
-              const option = {upsert: true}
-
-              const updatedDoc = {
-                $set: {
-                  name: userdata.name,
-                  bio: userdata.bio ? userdata.bio : null,
-                  profile_pic: userdata.profile_pic ? userdata.profile_pic : null,
-                  github_link: userdata.github_link ? userdata.github_link : null,
-                  portfolio_link: userdata.portfolio_link ? userdata.portfolio_link : null,
-                  hackerRank_link: userdata.hackerRank_link ? userdata.hackerRank_link : null,
-                  codeForce_link: userdata.codeForce_link ? userdata.codeForce_link : null,
-                  dribble_link: userdata.dribble_link ? userdata.dribble_link : null,
-                  linkedin_link: userdata.linkedin_link ? userdata.linkedin_link : null,
-                  facebook_link: userdata.facebook_link ? userdata.facebook_link : null,
-                  instagram_link: userdata.instagram_link ? userdata.instagram_link : null,
-                  twitter_link: userdata.twitter_link ? userdata.twitter_link : null
-                }
+        const userdata = req.body
+        const existUserInUsers = await usersCollection.findOne({username: userdata.username})
+        if(existUserInUsers){
+          const existUserInProfile = await userProfileCollection.findOne({username: userdata.username})
+          if(existUserInProfile){
+            const filter = {username: userdata.username}
+            const option = {upsert: true}
+            
+            const updatedDoc = {
+              $set: {
+                name: userdata.name,
+                bio: userdata.bio ? userdata.bio : null,
+                profile_pic: userdata.profile_pic ? userdata.profile_pic : null,
+                github_link: userdata.github_link ? userdata.github_link : null,
+                portfolio_link: userdata.portfolio_link ? userdata.portfolio_link : null,
+                hackerRank_link: userdata.hackerRank_link ? userdata.hackerRank_link : null,
+                codeForce_link: userdata.codeForce_link ? userdata.codeForce_link : null,
+                dribble_link: userdata.dribble_link ? userdata.dribble_link : null,
+                linkedin_link: userdata.linkedin_link ? userdata.linkedin_link : null,
+                facebook_link: userdata.facebook_link ? userdata.facebook_link : null,
+                instagram_link: userdata.instagram_link ? userdata.instagram_link : null,
+                twitter_link: userdata.twitter_link ? userdata.twitter_link : null
               }
-              const saveInfo = await userProfileCollection.updateOne(filter, updatedDoc, option)
-
-              return res.send(saveInfo)
             }
-            else{
-              const saveInfo = await userProfileCollection.insertOne(userdata)
-              return res.send(saveInfo)
-            }
-
+            const saveInfo = await userProfileCollection.updateOne(filter, updatedDoc, option)
+            
+            return res.send(saveInfo)
           }
-          else {
-            return res.status(400).json({error: 'Invalid user', code: 21})
+          else{
+            const saveInfo = await userProfileCollection.insertOne(userdata)
+            return res.send(saveInfo)
           }
-
+          
+        }
+        else {
+          return res.status(400).json({error: 'Invalid user', code: 21})
+        }
+        
       })
-
+      
       app.get('/userdata', async(req, res) => {
-          const query = {username: req.query.username}
-          const userdata = await userProfileCollection.findOne(query)
-          res.send(userdata)
+        const query = {username: req.query.username}
+        const userdata = await userProfileCollection.findOne(query)
+        res.send(userdata)
       })
 
+      app.get('/userAccountInfo', async(req, res) => {
+        const query = {
+            username: req.query.username,
+            password: req.query.pass
+        }
+        console.log(query)
+        
+        const userAccountInfo = await usersCollection.findOne(query)
+        res.send(userAccountInfo)
+      })
+      
       app.get('/profile/:username', async(req, res) => {
-          const query = {username: req.params.username}
+        const query = {username: req.params.username}
           const userData = await userProfileCollection.findOne(query)
           res.send(userData)
       })
