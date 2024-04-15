fa442e42463fad033636ada90bed1f8db0a0dd1f
Sujoy Paul <freelancersujoy@gmail.com>
2024-04-15 11:52:25 +0600

    up to date

--- profile-view/src/Pages/Home.jsx
+++ profile-view/src/Pages/Home.jsx
@@ -40,16 +40,18 @@ const Home = () => {
                     <p className='text-lg text-center '>Your account was created successfully and you are also logged in. Let's share the social links with <span className='font-bold text-[#e4bf39]'>PROFILE-VIEW</span></p>
 
                     <span className='mt-1 text-slate-300 text-sm uppercase'>Hey <span className='bg-[#340b1d] text-[#a42a5f] px-2 font-bold'>{user?.username}</span> Click on arrange profile button </span>
-                    <div className='flex md:flex-row items-center gap-3'>
-                        <button onClick={() => navigate('/editprofile')} className='px-5 py-2 md:w-[200px] btn-bg rounded-md border-[1px] border-[#242424] uppercase mt-10'>Arrange Profile</button>
+                    <div className=' mt-10 mx-auto w-full md:w-fit'>
+                    <div className='flex md:flex-row gap-3 flex-col items-center  '>
+                        <button onClick={() => navigate('/editprofile')} className='px-5 py-2 md:w-[200px] w-full btn-bg rounded-md border-[1px] border-[#242424] uppercase '>Arrange Profile</button>
                         {
                             username &&
-                        <button onClick={() => navigate(`/profile/${user.username}`)} className='px-5 py-2 md:w-[200px] btn-bg rounded-md border-[1px] border-[#242424] uppercase mt-10'>My Profile</button>
+                        <button onClick={() => navigate(`/profile/${user.username}`)} className='px-5 py-2 md:w-[200px] w-full btn-bg rounded-md border-[1px] border-[#242424] uppercase '>My Profile</button>
                         }
                         {
                             username &&
-                            <button onClick={() => handleLogout()} className='px-5 py-2 btn-bg rounded-md border-[1px] border-[#242424]  mt-10 flex items-center  gap-2'><CiLogout className='text-lg' /> Log out</button>
+                            <button onClick={() => handleLogout()} className='px-5 py-2 btn-bg rounded-md border-[1px] border-[#242424] md:w-[200px] w-full flex justify-center items-center gap-2'><CiLogout className='text-lg mt-1' /> Log out</button>
                         }
+                    </div>
                     </div>
                 </div>
                 :
