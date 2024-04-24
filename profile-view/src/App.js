dff9a4fb56466a1899cda5292ed9d37899593333
Sujoy Paul <freelancersujoy@gmail.com>
2024-04-24 21:20:00 +0600

    fixe: �🍃 background image lazy load fixed

--- profile-view/src/App.js
+++ profile-view/src/App.js
@@ -9,20 +9,20 @@ function App() {
   if(!localStorage.getItem('userinfo')) localStorage.setItem('userinfo', JSON.stringify([{}]))
   return (
     <div className="App">
-      {/* <RouterProvider router={routes}></RouterProvider>
+      <RouterProvider router={routes}></RouterProvider>
        <Toaster
        position="top-center"
        reverseOrder={false}
-     /> */}
+     />
 
-     <div className='h-screen flex items-center justify-center flex-col w-[80%] mx-auto overflow-hidden'>
+     {/* <div className='h-screen flex items-center justify-center flex-col w-[80%] mx-auto overflow-hidden'>
         <h1 className='md:text-[3rem] text-[4rem] text-center text-slate-200 '><span  className='text-[#e4bf39] font-bold uppercase'>Hey dev!</span> <br />"This page is getting a makeover. Check back soon. Stay tuned! "</h1>
         
         <div className='mt-5 text-center border-t-[1px]'>
           <p className='text-lg text-white mt-1'>The owner of this website is updating profileView to change something</p>
           <p className='uppercase'>Updates are in process.</p>
         </div>
-     </div>
+     </div> */}
 
     </div>
   );
