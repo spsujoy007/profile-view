7282c0aef20a495058938794cb7a34281c332cb9
Sujoy <sujoypaul728@gmail.com>
2026-02-03 12:28:25 +0530

    feat: add multer middleware for file uploads and update package dependencies

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './Public/temp')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + '-' + Date.now())
+  }
+})
+
+export const upload = multer({ 
+    storage: storage 
+})
